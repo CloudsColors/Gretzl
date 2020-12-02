25dabccba2c0839596f7d0e68ea3440951a0db12
Kam4 <k.mudy94@gmail.com>
2020-12-02 16:12:06 +0100

    Handling command line arguments

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
-import os, glob, sys, re, fileinput
+import os, glob, sys, re, fileinput, argparse
 
 '''
 Runs the whole script
 '''
 def main():
-    files = collect_files_from_directory("")
+    arguments = process_command_line()   
+    if(arguments.dpm == 'mvn'):
+        #Do something here 
+        mvn = True  
+        print(mvn)
+
+    files = collect_files_from_directory(arguments.d)
     for f in files:
         open_and_inject_code(f)
-    
+
+
+def process_command_line():
+    parser = argparse.ArgumentParser(description="Usage:")
+    parser.add_argument("-d", type=str, required=True,  help="Add path to dir")
+    parser.add_argument("-dpm", choices=["mvn", "gradle"], help="Choose used dependancy manager")
+    args = parser.parse_args()
+    return args
+
 '''
 Collects all the .java files in the path given by the argument pathToDirectory
 '''
 def collect_files_from_directory(pathToDirectory):
-    fileNames = glob.glob(r"C:\Users\Andreas\Desktop\Uni\Software evolution project\example_system\**\*.java", recursive=True)
+    fileNames = glob.glob(pathToDirectory +"\\**\\*.java", recursive=True)
     return fileNames
 
 '''
