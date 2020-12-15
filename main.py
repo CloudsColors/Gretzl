e6e0ed29baefd9af651eeded35d79cc65e45d9d4
johmara <johan.t.martinson@gmail.com>
2020-12-15 15:47:55 +0100

    beautify

--- main.py
+++ main.py
@@ -7,17 +7,15 @@ Runs the whole script
 files = []
 
 def main():
-    arguments = process_command_line()   
-    if(arguments.dpm == 'mvn'):
-        #Do something here 
-        mvn = True  
-        print(mvn)
+    arguments = process_command_line()  
+    files = collect_files_from_directory(arguments.d) 
+    if(arguments.delete):
+        for f in files:
+            remove_the_injected_code(f)
+    else:
+        for f in files:
+            open_and_inject_code(f)
 
-    files = collect_files_from_directory(arguments.d)
-    for f in files:
-        open_and_inject_code(f)
-    """ for f in files:
-        remove_the_injected_code(f) """
 
 '''
 Processes command line arguments
@@ -25,10 +23,9 @@ Processes command line arguments
 def process_command_line():
     parser = argparse.ArgumentParser(description="Usage:")
     parser.add_argument("-d", type=str, required=True,  help="Add path to dir")
-    parser.add_argument("-dpm", choices=["mvn", "gradle"], help="Choose used dependancy manager")
+    parser.add_argument("--delete", action='store_true', help="Delete all the injected code from code base")
     args = parser.parse_args()
     return args
-
 
 '''
 Collects all the .java files in the path given by the argument pathToDirectory
@@ -46,24 +43,17 @@ def open_and_inject_code(filePath):
         for line in f:
             if(re.search(r"((public|private|protected|class)(\s[A-Za-z\s]*)([^}]*)([{]))", line)):
                 if("class" in line):
-                    regex = r"((public\s|private\s|protected\s|class\s)|(implements\s[ A-Za-z]+|extends\s[ A-Za-z]+)|([{]))"
+                    regex = r"((public\s|private\s|protected\s|class\s)|(implements\s[A-Za-z]+|extends\s[A-Za-z]+)|([{]))"
                     name = re.sub(regex, "", line)
-                    line = "import com.CloudsColors.Gretzl.Gretzl; \n\n" + line + "\tGretzl gretzlLogger = new Gretzl(\""+name.strip("\n")+"\");"
+                    line = "import com.CloudsColors.Gretzl.Gretzl; \n\n" + line + "\tstatic Gretzl gretzl = new Gretzl(\""+name.strip(" \n")+"\");"
                     print(line, end="\n")
                 else:
                     regex = r"((public\s|private\s|protected\s)|(throws\s[A-Za-z]+)|([{]))"
                     name = re.sub(regex, "", line)
-                    line = line + "\t\tgretzlLogger.log(\""+name.strip()+"\");"
+                    line = line + "\t\tgretzl.log(\""+name.strip()+"\");"
                     print(line, end="\n")
             else:
                 print(line, end="")
-
-'''
-Executes the Java system that the tool will explore
-'''
-def execute_system():
-    #execute the java project
-    return
 
 '''
 Remove the previously injected code, cleanup!
@@ -71,7 +61,7 @@ Remove the previously injected code, cleanup!
 def remove_the_injected_code(path):
     with fileinput.FileInput(path, inplace=True) as f:
         for line in f:
-            if(re.search(r"((Gretzl\sgretzlLogger\s=\snew\sGretzl[(-;]*|gretzlLogger.log[(-;]*))", line)):
+            if(re.search(r"((Gretzl\sgretzl\s=\snew\sGretzl[(-;]*|gretzl.log[(-;]*))|import com.CloudsColors.Gretzl.Gretzl;", line)):
                 line = ""
             print(line, end="")
     return
